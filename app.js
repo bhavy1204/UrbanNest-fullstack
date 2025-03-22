d5c21d26f2f679acae52998ecfa4fadcab954922
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-22 10:07:26 +0530

    Database setup completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 
 const app = express();
+const Listing = require("./models/listing.js");
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/UrbanNest');
@@ -10,6 +11,20 @@ main().then(()=>{
     console.log("MongoDB connnected");
 }).catch((err) => {
     console.log(err);
+});
+
+app.get("/testing", async (req,res)=>{
+    let sampleData = new Listing({
+        title :"My shop",
+        description :"In main road bapubazar",
+        price:20000,
+        location:"Udaipur",
+        country:"India",
+    });
+    await sampleData.save().then(()=>{
+        console.log("Data saved");
+    });
+    res.send("Success");
 });
 
 app.get("/",(req,res)=>{
---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        default :"https://images.unsplash.com/photo-1610513320995-1ad4bbf25e55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        type: String,
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1610513320995-1ad4bbf25e55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;

d277aa3f79d49dc430744ec29355b3983349aea0
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-25 08:39:51 +0530

    Completed CRUD routes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
-
 const app = express();
-app.use(express.urlencoded({extended:true}));
 const Listing = require("./models/listing.js");
-
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
+app.use(express.urlencoded({extended:true}));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/UrbanNest');
@@ -18,7 +18,9 @@ main().then(()=>{
 });
 
 app.set("view engine","ejs");
+app.engine('ejs', ejsMate);
 app.set("views",path.join(__dirname,"views"));
+app.use(methodOverride ("_method"));
 
 // app.get("/testing", async (req,res)=>{
 //     let sampleData = new Listing({
@@ -56,11 +58,37 @@ app.get("/listings/:id",async (req,res)=>{
 // CREATE ROUTE
 app.post("/listings",async (req,res)=>{
     // let {title,description,image,price,location,country} = req.body;
-    let {listing} = req.body;
-    await Listing.insertOne(listing);
-    console.log("Data was inserted ");
+    // let {listing} = req.body;
+    // await Listing.insertOne(listing);
+    // console.log("Data was inserted ");
+    // res.redirect("/listings");
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    console.log(newListing);
     res.redirect("/listings");
 });
+
+
+// Update route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const listing =  await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+// DELETE ROUTE 
+app.delete("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+}); 
 
 app.get("/",(req,res)=>{
     res.send("Home route");

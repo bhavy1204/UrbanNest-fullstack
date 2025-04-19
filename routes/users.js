9808537f7ce7aecb01e4e334b78ee4e094b78345
bhavy1204 <bhavysarwa51@gmail.com>
2025-04-19 19:33:33 +0530

    feat: login and signup post req completed

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const { route } = require("./listing");
-const router = express.Router({mergeParams:true}); //....? 
+const router = express.Router({ mergeParams: true }); //....? 
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     // res.send("form WIll come soon ! ");
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req,res)=>{
-    try{ 
-        let {username,email,password} = req.body;
-        const newUser = new User({email,username});
-        const registerdUser = await User.register(newUser,password);
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registerdUser = await User.register(newUser, password);
         console.log(registerdUser);
-        req.flash("success","registerd Successfully !");
+        req.flash("success", "registerd Successfully !");
         res.redirect("/listings");
-    }catch(err){
-        req.flash("error",err.message);
+    } catch (err) {
+        req.flash("error", err.message);
         res.redirect("/signup");
     } //we did try catch bcz wrapAsync was handling request but it was taking us to a different page altorgter but with help of try catch we can just flash a message for the error 
 }));
 
 
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
     res.render("users/login.ejs")
-})
+});
+
+router.post("/login", 
+    passport.authenticate("local", 
+    { failureRedirect: "/login", 
+    failureFlash: true }), 
+    async (req, res) => {
+        req.flash("success","logged in successfully");
+        res.redirect("/listings");
+    }
+);
 
 module.exports = router;

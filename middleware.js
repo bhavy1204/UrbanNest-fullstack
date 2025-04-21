84e58881bca8ea135fc538c243a18ca34df422d3
bhavy1204 <bhavysarwa51@gmail.com>
2025-04-21 10:46:30 +0530

    fix: fix correct redirect after login and signup

--- middleware.js
+++ middleware.js
@@ -2,8 +2,17 @@ module.exports.isloggedin=  (req,res,next)=>{
     // console.log(req.user); // jo request hoti hai usme user related information saved hoti hai. 
     //We want to check if the user is logged in before creatinh new listing so 
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error","Please log in to create listing");
         return res.redirect("/login");
+    }
+    next();
+}
+
+
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,27 @@ const router = express.Router({ mergeParams: true }); //....?
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
     // res.send("form WIll come soon ! ");
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req, res) => {
+router.post("/signup", wrapAsync(async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registerdUser = await User.register(newUser, password);
-        // console.log(registerdUser);
-        req.flash("success", "registerd Successfully !");
-        res.redirect("/listings");
+        // It will login the user just after signup.
+        req.login(registerdUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            // console.log(registerdUser);
+            req.flash("success", "registerd Successfully !");
+            res.redirect(req.session.redirectUrl);
+        });
     } catch (err) {
         req.flash("error", err.message);
         res.redirect("/signup");
@@ -29,23 +36,29 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs")
 });
 
-router.post("/login", 
-    passport.authenticate("local", 
-    { failureRedirect: "/login", 
-    failureFlash: true }), 
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local",
+        {
+            failureRedirect: "/login",
+            failureFlash: true
+        }),
     async (req, res) => {
-        req.flash("success","logged in successfully");
-        res.redirect("/listings");
+        req.flash("success", "logged in successfully");
+        // res.redirect(req.session.redirectUrl); //Isme ek problem hai ki jese hi user login hua aur success msg aaya to fir vo session ko reset kar deta hai.. To isse apne ko passport error dega isliye apan isko locals me store karate ha.
+        // res.redirect(res.locals.redirectUrl);
+        let redirectUrl = res.locals.redirectUrl || "/listings"; //This will prevent error when user tries to login in home page only.
+        res.redirect(redirectUrl);
     }
 );
 
 // Logout.. It will delete user info from session making it logout.. With help of passport
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
             next(err);
         }
-        req.flash("success","logged out");
+        req.flash("success", "logged out");
         res.redirect("/listings");
     })
 });

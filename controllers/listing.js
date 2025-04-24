26badcfe39a3ed138abc8f6dca0e2cee6076b9ae
bhavy1204 <bhavysarwa51@gmail.com>
2025-04-24 12:58:04 +0530

    feat: completed upload of images as files and display them, using env and cloudinary

--- controllers/listing.js
+++ controllers/listing.js
@@ -33,8 +33,11 @@ module.exports.createListing = async (req, res) => {
     // await Listing.insertOne(listing);
     // console.log("Data was inserted ");
     // res.redirect("/listings");
+    let url = req.file.path;
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id; //To create a listing with the logged in user id.
+    newListing.image = {url,filename};
     await newListing.save();
     req.flash("success","Listing Added Successfully");
     res.redirect("/listings");
--- routes/listing.js
+++ routes/listing.js
@@ -5,24 +5,24 @@ const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("../schema.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
 const { isloggedin } = require("../middleware.js");
-const {isowner} = require("../middleware.js");
+const { isowner } = require("../middleware.js");
 
 // controllers
 const listingContoller = require("../controllers/listing.js");
 
 // For multipart form data parsing
-const multer  = require('multer');
-const {storage} = require("../cloudConfig.js");
-const upload = multer({ storage }); //multer will store files by deafult in "storage"
+const multer = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage }); //multer will store files by deafult in storage"
 
 // SERVER SIDE VALIDATION FOR LISTINGS
-const validateListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(", ");
-        throw new ExpressError(400,errMsg);
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
     }
     else
         next();
@@ -32,25 +32,22 @@ const validateListing = (req,res,next)=>{
 router.get("/", wrapAsync(listingContoller.index));
 
 // NEW LISTING ROUTE
-router.get("/new",  isloggedin, listingContoller.renderNewForm);
+router.get("/new", isloggedin, listingContoller.renderNewForm);
 
 // SHOW ALL ROUTE
 router.get("/:id", wrapAsync(listingContoller.showListing));
 
 // CREATE ROUTE
-router.post("/",upload.single('listing[image]'),(req,res)=>{
-    // res.send(req.body);
-    res.send(req.file); // in multer
-});//("/", isloggedin,validateListing, wrapAsync(listingContoller.createListing));
+router.post("/", isloggedin, upload.single('listing[image]'),validateListing, wrapAsync(listingContoller.createListing));
 
 
 // Update route
-router.get("/:id/edit", isloggedin,isowner,wrapAsync(listingContoller.renderUpdateForm));
+router.get("/:id/edit", isloggedin, isowner, wrapAsync(listingContoller.renderUpdateForm));
 
-router.put("/:id", isloggedin,isowner,validateListing, wrapAsync(listingContoller.updateListing));
+router.put("/:id", isloggedin, isowner, validateListing, wrapAsync(listingContoller.updateListing));
 
 // DELETE ROUTE 
-router.delete("/:id",isloggedin, isowner,wrapAsync(listingContoller.deleteListing));
+router.delete("/:id", isloggedin, isowner, wrapAsync(listingContoller.deleteListing));
 
 module.exports = router;
 
